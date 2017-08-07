4a62b4596abf3a569abe347efe03711f0623ad16
Victor Couty <victor@couty.eu>
2017-08-07 18:16:48 +0200

    Added documentation

--- superdoku.py
+++ superdoku.py
@@ -3,10 +3,23 @@
 import numpy as np
 
 class UnsolvableError(Exception):
+  """The exception to raise if the grid cannot be solved"""
   pass
 
 class Grid(object):
+  """
+  This class represents a Sudoku grid
+
+  It takes the base array (a numpy array), a list of the authorized values and
+  the value that represents an empty box.
+
+  It implements the methods necessary for adeterministic resolution.
+  """
   def __init__(self,table,choices,empty=0):
+    """
+    Basic checks to make sure the input array is correct and
+    extracts the size
+    """
     self.table = table
     self.choices = choices
     self.empty = empty
@@ -18,11 +31,19 @@ class Grid(object):
     self.check_validity()
 
   def subzone(self,y,x):
+    """
+    Returns the subzone of a box (in which only one of each vaue is allowed)
+    """
     b = (y//self.primary)*self.primary
     a = (x//self.primary)*self.primary
     return self.table[b:b+self.primary,a:a+self.primary]
 
   def get_values(self,y,x):
+    """
+    Returns the allowed values for an empty box
+
+    If the box is filled, returns the value of the box
+    """
     if self.table[y,x] != self.empty:
       return self.table[y,x]
     r = list(self.choices)
@@ -44,6 +65,11 @@ class Grid(object):
     return r
 
   def fill(self):
+    """
+    Fills all the boxes that have only one option left
+
+    Returns True if anything changed, False otherwise
+    """
     left = self.table == self.empty
     old = left.copy()
     for i in range(self.size):
@@ -61,6 +87,11 @@ class Grid(object):
     return (old != left).any()
 
   def check_validity(self):
+    """
+    To check if the grid is valid
+
+    It raises an assertion error if this grid is invalid
+    """
     for i in range(self.size):
       l = self.table[i,:][self.table[i,:]!=self.empty]
       assert len(set(l)) == len(l),"Invalid grid"
@@ -72,13 +103,26 @@ class Grid(object):
         assert len(set(l)) == len(l),"Invalid grid"
 
   def iter_fill(self):
+    """
+    Fills the grid until it no longer changes anything
+    """
     while self.fill():
       pass
 
   def solved(self):
+    """
+    Returns True if the grid is solved
+    """
     return not self.empty in self.table
 
   def get_hypothesis(self):
+    """
+    Returns coordinates of the box where the hypothesis are the cheapest
+
+    It looks for the box with least possible values (2 at best)
+    It asserts that the box was iter_filled and therefore there are
+    no box with only one possibility
+    """
     lowest = len(self.choices)+1
     for i in range(self.size):
       for j in range(self.size):
@@ -99,6 +143,14 @@ class Grid(object):
 # ==============
 
 def solve(g):
+  """
+  Takes a grid object and tries to solve it
+
+  It is a recursive function that looks for a solution by filling the
+  grid and making hypothesis if necessary.
+
+  It raises UnsolvableError if there are no solutions
+  """
   g.iter_fill()
   if g.solved():
     return g
@@ -109,12 +161,18 @@ def solve(g):
     try:
       r = solve(Grid(new_a,g.choices,g.empty))
     except UnsolvableError:
-      pass
+      continue # Run the loop with another value
     else:
       if r.solved():
-        return r
+        return r # Returns the solved one
+  # If none of the hypothesis worked, it is unsolvable
+  # Maybe the previous hypothesis is the cause
+  # So it wil enter the except and loop on antoher value
   raise UnsolvableError
 
 def solve_arr(arr,choices=list(range(1,10)),empty=0):
+  """
+  To instanciate the Grid object, to hide it to the user
+  """
   g = Grid(arr,choices,empty)
   return solve(g).table
